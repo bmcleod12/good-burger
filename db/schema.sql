bc7303abe4468c54052c74a4976a279344a0b680
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-03-10 18:15:47 -0600

    general troubleshooting - cannot POST; going to copy cat project and start over

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
     burger VARCHAR(50) NOT NULL,
-    devoured BIT,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
